4b6a4ee2483ca1399bab05e8b42807461bbe4ba0
Sumit Srivastava9 <sumit.srivastava9@ibm.com>
2023-05-05 00:45:35 +0530

    test_new

---
+++ app.py
@@ -0,0 +1,28 @@
+#took help from https://www.youtube.com/watch?v=Z1RJmh_OqeA
+
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+
+@app.route('/')
+def index():
+    # return "Hello World! from Flask"
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    

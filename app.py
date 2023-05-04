4340a62ec3c0cd9fa36719fd65b7fb38a15b5e8d
Sumit Srivastava9 <sumit.srivastava9@ibm.com>
2023-05-05 02:07:25 +0530

    test_new

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-#took help from https://www.youtube.com/watch?v=Z1RJmh_OqeA
+import cv2
 
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+# Function to extract frames
+def FrameCapture(path):
+    # Path to video file
+    vidObj = cv2.VideoCapture(path)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+    # Used as counter variable
+    count = 0
 
-class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    # checks whether frames were extracted
+    success = 1
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
-    
+    while success:
+        # vidObj object calls read
+        # function extract frames
+        success, image = vidObj.read()
 
-@app.route('/')
-def index():
-    # return "Hello World! from Flask"
-    return render_template('index.html')
+        if success:
+            # Check if the frame is a multiple of 10
+            if count % 60 == 0:
+                # Save the frame with frame-count
+                cv2.imwrite("./frame/frame%d.jpg" % count, image)
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    
+            count += 1
+
+    # Release the video file object and close all windows
+    vidObj.release()
+    cv2.destroyAllWindows()
+
+# Driver Code
+if __name__ == '__main__':
+    # Calling the function
+    FrameCapture("./frame/test_new.mp4")
--- mp42frame.py
+++ mp42frame.py
@@ -1,33 +1,47 @@
-# Program To Read video
-# and Extract Frames
-
 import cv2
 
 # Function to extract frames
 def FrameCapture(path):
-	# Path to video file
-	vidObj = cv2.VideoCapture(path)
- 	
-	# Used as counter variable
-	count = 0
-	# checks whether frames were extracted
-	success = 1
+    # Path to video file
+    vidObj = cv2.VideoCapture(path)
 
-	while success:
-		# vidObj object calls read
-		# function extract frames
-		success, image = vidObj.read()
-  
-		# Saves the frames with frame-count
-		# cv2.imwrite("/Users/Suranjan/Kathal/frame%d.jpg" % count, image)
-		count += 1
+    # Used as counter variable
+    count = 0
+    # Time delay in milliseconds between frame captures
+    time_delay = 10000
+    # Current time
+    current_time = 0
 
-	cv2.imwrite("/Users/Suranjan/Kathal/frame%d.jpg" % count, image)  
-#	vidObj.release()
-#cv2.destroyAllWindows()
-	
+    # checks whether frames were extracted
+    success = 1
+
+    while success:
+        # vidObj object calls read
+        # function extract frames
+        success, image = vidObj.read()
+
+        if success:
+            # Check if 10 seconds have passed since the last frame capture
+            if count == 0 or (cv2.waitKey(1) & 0xFF == ord('q')) or (cv2.waitKey(1) & 0xFF == ord('n')) or (cv2.waitKey(1) & 0xFF == ord('p')) or (cv2.waitKey(1) & 0xFF == ord('s')):
+                if count != 0:
+                    # Update the current time
+                    current_time += time_delay
+
+                # Save the frame with frame-count
+                cv2.imwrite("./frame/frame%d.jpg" % count, image)
+                count += 1
+
+                # Update the current time
+                current_time = current_time + time_delay
+
+                # Set the video position to the current time
+                vidObj.set(cv2.CAP_PROP_POS_MSEC, current_time)
+
+    # Release the video file object and close all windows
+    vidObj.release()
+    cv2.destroyAllWindows()
+
 # Driver Code
 if __name__ == '__main__':
-
-	# Calling the function
-	FrameCapture("/Users/Suranjan/Kathal.mp4")
+    # Calling the function
+    FrameCapture("./frame/test1.mp4")

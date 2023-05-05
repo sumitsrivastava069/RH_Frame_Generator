97631bf4690969fd4836cdce34b8d8429bb3ad90
Sumit Srivastava9 <sumit.srivastava9@ibm.com>
2023-05-05 15:55:30 +0530

    test

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
 import cv2
+from flask import Flask
 
-# Function to extract frames
+app = Flask(__name__)
+
 def FrameCapture(path):
-    # Path to video file
+    
     vidObj = cv2.VideoCapture(path)
 
     # Used as counter variable
     count = 0
 
-    # checks whether frames were extracted
+    
     success = 1
 
     while success:
-        # vidObj object calls read
-        # function extract frames
+        # vidObj object calls read function to extract frames
         success, image = vidObj.read()
 
         if success:
             # Check if the frame is a multiple of 10
             if count % 60 == 0:
-                # Save the frame with frame-count
-                cv2.imwrite("./frame/frame%d.jpg" % count, image)
+                
+                cv2.imwrite("./frame_new/frame%d.jpg" % count, image)
 
             count += 1
     while not success:
         print("No Frame Found")
 
-    # Release the video file object and close all windows
+    
     vidObj.release()
     cv2.destroyAllWindows()
 
-# Driver Code
+
 if __name__ == '__main__':
-    # Calling the function
-    FrameCapture("./frame/test_new.mp4")
+    path = "./frame/test_new.mp4"
+    FrameCapture(path)
+
+    
+    app.run()

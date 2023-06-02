9345eb203384eb247d7466ca018c7132d2a07882
sumitsrivastava069 <61084331+sumitsrivastava069@users.noreply.github.com>
2023-06-02 13:31:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import os
 import cv2
 import time
 from flask import Flask
+import csv
+
+def write_to_csv(file_name):
+    csv_file = "./storage/Frames/processed_files.csv"
+    with open(csv_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([file_name])
 
 
 app = Flask(__name__)
@@ -22,8 +29,8 @@ def frame_capture(path):
 
         if success:
             # Check if the frame is a multiple of 10
-            if count % 240 == 0:
-                cv2.imwrite("./storage/Frame/frame%d.jpg" % count, image)
+            if count % 60 == 0:
+                cv2.imwrite("./storage/Frames/frame%d.jpg" % count, image)
 
             count += 1
 
@@ -32,24 +39,27 @@ def frame_capture(path):
 
 
 def monitor_folder():
-    
     folder_path = "./storage/CCTV_Capture"
-    
-    processed_files = set()
+    csv_file = "./storage/Frames/processed_files.csv"
+
+    # Load existing file names from CSV
+    existing_files = set()
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            existing_files.add(row[0])
 
     while True:
-        
         # get all files in the folder
         files = os.listdir(folder_path)
 
         # check for new files
         for file_name in files:
-            if file_name.endswith(".mp4") and file_name not in processed_files:
-                
+            if file_name.endswith(".mp4") and file_name not in existing_files:
                 file_path = os.path.join(folder_path, file_name)
                 frame_capture(file_path)
-                processed_files.add(file_name)
-                
+                write_to_csv(file_name)
+                existing_files.add(file_name)
                 print("New video file received: ", file_name)
 
         # sleep for a few seconds before checking again

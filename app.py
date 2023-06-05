e30ba6e0bf5fe99f8caec99847e464445e94f2d4
sumitsrivastava069 <61084331+sumitsrivastava069@users.noreply.github.com>
2023-06-06 00:22:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2
 import time
 from flask import Flask
 import csv
+import logging
 
 def write_to_csv(file_name):
     csv_file = "./storage/Frame/processed_files.csv"
@@ -41,7 +42,6 @@ def frame_capture(path):
 def monitor_folder():
     folder_path = "./storage/CCTV_Capture"
     csv_file = "./storage/Frame/processed_files.csv"
-
     # Load existing file names from CSV
     existing_files = set()
     with open(csv_file, 'r') as file:
@@ -49,23 +49,28 @@ def monitor_folder():
         for row in reader:
             existing_files.add(row[0])
 
+    logging.basicConfig(filename='error.log', level=logging.ERROR,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
     while True:
         # get all files in the folder
         files = os.listdir(folder_path)
 
         # check for new files
         for file_name in files:
-            if file_name.endswith(".mp4") and file_name not in existing_files:
-                file_path = os.path.join(folder_path, file_name)
-                frame_capture(file_path)
-                write_to_csv(file_name)
-                existing_files.add(file_name)
-                print("New video file received: ", file_name)
+            try:
+                if file_name.endswith(".mp4") and file_name not in existing_files:
+                    file_path = os.path.join(folder_path, file_name)
+                    frame_capture(file_path)
+                    write_to_csv(file_name)
+                    existing_files.add(file_name)
+                    print("New video file received: ", file_name)
+            except Exception as e:
+                logging.error(str(e))
 
         # sleep for a few seconds before checking again
         time.sleep(5)
 
-
+app = Flask(__name__)
 if __name__ == '__main__':
     monitor_folder()
     
